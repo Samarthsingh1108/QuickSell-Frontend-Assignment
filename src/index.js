7a3e051238ae3f5e09528af88618a6df58833e1e
SAMARTH SINGH <111498529+Samarthsingh1108@users.noreply.github.com>
2024-10-23 01:19:53 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+//Samarth Singh (21BEC2251)
+
+//Samarth Singh (21BEC2251)
+
+reportWebVitals();
---
+++ src/setupTests.js
@@ -0,0 +1,4 @@
+
+import '@testing-library/jest-dom';
+//Samarth Singh (21BEC2251)
+//Samarth Singh (21BEC2251)
